ef1165dae2365bc28b67db9a071e25165e0754f4
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-27 12:07:55 -0700

    Da formato a script para llenar la base de datos

--- scripts/database/python/fillDataBase.py
+++ scripts/database/python/fillDataBase.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 # Script para agregar los datos de Excel a la base de datos
 
 import sqlite3
 from sqlite3 import Error
 
-import pandas as pd
 import numpy as np
+import pandas as pd
+
 
 def create_connection(db_file):
     try:
@@ -14,37 +16,42 @@ def create_connection(db_file):
         print(e)
     return None
 
+
 def select_island_by_name(conexion, nombre):
     cursor = conexion.cursor()
-    cursor.execute('SELECT * FROM isla WHERE nombre=?', (nombre,)) 
+    cursor.execute('SELECT * FROM isla WHERE nombre=?', (nombre,))
     isla = cursor.fetchone()
     return isla
 
+
 def select_species_by_name(conexion, nombre):
     cursor = conexion.cursor()
-    cursor.execute('SELECT * FROM especie WHERE nombre=?', (nombre,)) 
+    cursor.execute('SELECT * FROM especie WHERE nombre=?', (nombre,))
     especie = cursor.fetchone()
     return especie
+
 
 def create_registry(conexion, renglon):
     nombre_isla = renglon['Isla/Grupo islas']
     nombre_especie = renglon['Especie en inglés']
-    maximo_numero_individuos = renglon['Máxima cantidad o cantidad total de nidos (parejas)']
+    maximo_numero_individuos = renglon[
+        'Máxima cantidad o cantidad total de nidos (parejas)']
     temporada = renglon['Año']
     notas = renglon['Notas']
     id_isla = select_island_by_name(conexion, nombre_isla)[0]
     id_especie = select_species_by_name(conexion, nombre_especie)[0]
     comando_SQL = ''' INSERT INTO registro (id_isla, id_especie, temporada, maximo_nidos, notas)
         VALUES(?,?,?,?,?) '''
-    isla = (id_isla, id_especie, temporada, maximo_numero_individuos, notas)    
+    isla = (id_isla, id_especie, temporada, maximo_numero_individuos, notas)
     cursor = conexion.cursor()
     cursor.execute(comando_SQL, isla)
     return cursor.lastrowid
- 
+
+
 if __name__ == '__main__':
     conexion = create_connection('./aves_marinas.db')
-    tabla_datos = pd.read_excel('./base_datos_parejas_aves_marinas_islas.xlsx').dropna(subset=['Isla/Grupo islas'])
+    tabla_datos = pd.read_excel(
+        './base_datos_parejas_aves_marinas_islas.xlsx').dropna(subset=['Isla/Grupo islas'])
     with conexion:
         for indice, renglon in tabla_datos.iterrows():
             project_id = create_registry(conexion, renglon)
-    
