2b51df696cb95a7c609e063eefef91ad3483b94c
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-27 13:31:01 -0700

    Agrega documentación a las funciones

--- web/static/js/colors.js
+++ web/static/js/colors.js
@@ -1,9 +1,19 @@
-function scaleToColor(x, out_range = [0, 255]) {
-    domain = ["A".charCodeAt(0), "Z".charCodeAt(0)];
-    y = (x - (domain[1] + domain[0]) / 2) / (domain[1] - domain[0]);
-    return y * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2;
+/**
+ * Regresa un valor entre 0 y 255 para cada letra del abecedario en mayúscula
+ * @param {char} letter Letra del abecedario en mayúscula.
+ * @param {[number, number]} out_range Valor mínimo y máximo de la escala de color.
+ */
+function scaleToColor(letter, out_range = [0, 255]) {
+    let dominio = ["A".charCodeAt(0), "Z".charCodeAt(0)];
+    let parcial = (letter - (dominio[1] + dominio[0]) / 2) / (dominio[1] - dominio[0]);
+    return parcial * (out_range[1] - out_range[0]) + (out_range[1] + out_range[0]) / 2;
 }
 
+/**
+ * Regresa una cadena que representa un color rgb a partir del código de cuatro
+ * letras.
+ * @param {string} code Código de cuatro letras del ave.
+ */
 function getRGBStringFromCode(code) {
     let valoresRGB = new Array;
     for (let letra of code) {
