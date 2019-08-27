64ac3fd84dded1796a11f990c781ee8c2d8e2747
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-08-27 12:31:33 -0700

    Da formato y documenta funciones para crear círculos

--- web/static/js/circle.js
+++ web/static/js/circle.js
@@ -1,8 +1,29 @@
-const radioTierra_m = 6371000;
+/**
+ * Crea un círculo alrededor de una coordenada especificada
+ * @param {number} radio_m Radio (m) del circulo que se va a crear
+ * @param {[number, number]} centro Coordenadas [lon, lat] del centro del círculo
+ * Referencia: https://stackoverflow.com/questions/45421774/how-to-draw-circles-with-radii-given-in-kilometers-accurately-on-world-map
+ */
 function createCircle(radio_m, centro) {
-    let circumference = radioTierra_m * Math.PI * 2;
-    let grados = radio_m / circumference * 360;
-    let circle = d3.geoCircle().center(centro).radius(grados);
-    return circle
+    const radioTierra_m = 6371000;
+    let circunferencia = radioTierra_m * Math.PI * 2;
+    let grados = (radio_m / circunferencia) * 360;
+    let circulo = d3.geoCircle()
+        .center(centro)
+        .radius(grados);
+    return circulo;
 }
-// Referencia: https://stackoverflow.com/questions/45421774/how-to-draw-circles-with-radii-given-in-kilometers-accurately-on-world-map
+
+/**
+ * Crea un círculo con las coordenadas en el formato que las espera google maps
+ * @param {*} radio_m Radio (m) del circulo que se va a crear
+ * @param {*} centro Coordenadas [lon, lat] del centro del círculo
+ */
+function createCircleForGoogleMaps(radio_m, centro) {
+    let coordenadas = createCircle(radio_m, centro)().coordinates[0];
+    let coordenadasConFormato = new Array();
+    for (let coordenada of coordenadas) {
+        coordenadasConFormato.push({ "lat": coordenada[1], "lng": coordenada[0] });
+    }
+    return coordenadasConFormato;
+}
