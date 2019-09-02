2abb309aff1c6a7dd51760a9fe638d4aa14f2b34
Jesus Efren Cabrera Ortega <efren507.507@gmail.com>
2019-09-02 16:39:55 -0700

    Cambia lugar donde se crea la tabla

--- front/public_html/js/main.js
+++ front/public_html/js/main.js
@@ -14,6 +14,45 @@ var circulos = new Array();
  */
 d3.select(window).on("load", async () => {
     let temporadas = await getSeasons();
+    let especies = await getBirds();
+    let islas = await getIslands();
+    for (let especie of especies) {
+        for (let isla of islas) {
+            $("#bird-list").append(`
+                <tr style="background-color: ${getRGBStringFromCode(
+                    especie.Codigo
+                )}">
+                    <th>${isla.Nombre}</th>
+                    <th>${especie.Nombre}</th>
+                    <th id="maximo-nidos-especie-${especie.Codigo}-${isla.Nombre.replace(" ", "")}"> - </th>
+                    <th id="lambda-especie-${especie.Codigo}-${isla.Nombre.replace(" ", "")}" style="background-color:black;color:orange">0.00</th>
+                </tr>
+            `);
+            (async (isla)=>{
+                let tablaHistorica = await getHistoric(especie.Codigo, isla.Nombre)
+                let arregloTemporada = tablaHistorica.map(function(value,index) {return value.Temporada;});
+                let arregloMaximoNidos = tablaHistorica.map(function(value,index) {return value.MaximoNidos;});
+                let lambda = await getLambda(arregloTemporada, arregloMaximoNidos);
+                let colorTexto = "orange";
+                if (lambda.lambda != "nan") {
+                    if (parseFloat(lambda.lambda) >= 1) {
+                        colorTexto = "green";
+                    }
+                    if (parseFloat(lambda.lambda) > 0 && parseFloat(lambda.lambda) < 1) {
+                        colorTexto = "yellow";
+                    }
+                    if (parseFloat(lambda.lambda) < 0) {
+                        colorTexto = "red";
+                    }
+                    $(`#lambda-especie-${especie.Codigo}-${isla.Nombre.replace(" ", "")}`).text(lambda.lambda.toFixed(2));
+                } else {
+                    colorTexto = "white";
+                    $(`#lambda-especie-${especie.Codigo}-${isla.Nombre.replace(" ", "")}`).text(lambda.lambda);
+                }
+                $(`#lambda-especie-${especie.Codigo}-${isla.Nombre.replace(" ", "")}`).css("color", colorTexto);
+            })(isla)
+        }
+    }
     temporadaActual = temporadas[indiceTemporada];
     let centroMapa = {"lat": 27.577622, "lng":-111.454526};
 
