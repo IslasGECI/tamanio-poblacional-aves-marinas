0f824c7df3c026e1288b0a9044847b46ffcf1bc7
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-04-16 12:56:57 -0700

    Elimina espacio innecesario

--- api-datos/cmd/tablacruce/main.go
+++ api-datos/cmd/tablacruce/main.go
@@ -3,10 +3,9 @@ package main // import "bitbucket.org/IslasGECI/tablacruce"
 
 import (
 	"database/sql"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 type App struct {
