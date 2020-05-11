d905c1ca20fcb38134f922f91b31aa208d4bc6e9
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-05-11 12:09:58 -0700

    Cambia nombre de función a inglés para que estén en el mismo idioma

--- api-lambdas/lambdas/get_lambda.py
+++ api-lambdas/lambdas/get_lambda.py
@@ -2,12 +2,12 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 
-def ley_potencia(T, Lambda, No):
+def power_law(T, Lambda, No):
     return No * np.power(Lambda, T)
 
 def get_lambdas(temporadas, maximo_nidos):
     temporadas = np.array(temporadas)
     numero_agno = temporadas - temporadas[0]
     maximo_nidos = np.array(maximo_nidos)
-    popt, pcov = curve_fit(ley_potencia, numero_agno.astype('float64'), maximo_nidos.astype('float64'))
+    popt, pcov = curve_fit(power_law, numero_agno.astype('float64'), maximo_nidos.astype('float64'))
     return popt
