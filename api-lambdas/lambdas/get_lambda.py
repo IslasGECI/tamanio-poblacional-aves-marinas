848c3a3b024de7e816e437b66852f67c36260231
Nepo <braulio.rojas@islas.org.mx>
2020-07-13 11:02:14 -0700

    Atiende sugerencias de lint

--- api-lambdas/lambdas/get_lambda.py
+++ api-lambdas/lambdas/get_lambda.py
@@ -2,13 +2,13 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 
-def power_law(T, Lambda, No):
-    return No * np.power(Lambda, T)
+def power_law(time, lambdas, n_o):
+    return n_o * np.power(lambdas, time)
 
 
 def get_lambdas(temporadas, maximo_nidos):
     temporadas = np.array(temporadas)
     numero_agno = temporadas - temporadas[0]
     maximo_nidos = np.array(maximo_nidos)
-    popt, pcov = curve_fit(power_law, numero_agno.astype("float64"), maximo_nidos.astype("float64"))
+    popt, _ = curve_fit(power_law, numero_agno.astype("float64"), maximo_nidos.astype("float64"))
     return popt
