d10cf6a47e5b4213f24962bd94b3f9ea368a3ca9
Xavier Paredes <xavier.paredes@islas.org.mx>
2021-01-27 16:38:23 -0700

    ✅🤡 inicializa una primera prueba de jugete

---
+++ front/tests/tests_functions.test.js
@@ -0,0 +1,7 @@
+const answer = require("./public_html/js/main.js");
+
+describe("Que vamos a probar?", function(){
+    it("escenario 1", function(){
+      expect(answer.setSeason()).
+    });
+});
